41464f80b7e47e8a4b0cc74244fa8b69b849968f
Hakanni <66316345+webdev32@users.noreply.github.com>
2020-07-26 12:18:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unit-5
+ Interactive Photo- Gallery
---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const baguetteBox = require('baguettebox.js');
+
+const search = document.querySelector('#search');
+const caption = document.querySelectorAll('.data-caption');
+const handleSearch = event => {
+    
+}
