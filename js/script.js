4e90aedd86328f00b51b5378553fd0385f73a410
Hakanni <66316345+webdev32@users.noreply.github.com>
2020-07-26 14:57:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // const baguetteBox = require('baguettebox.js');
 
-const search = document.querySelector('#search');
-const caption = document.querySelectorAll('.data-caption');
-const handleSearch = event => {
-    
-}
+// const search = document.querySelector('#search');
+// const caption = document.querySelectorAll('.gallery a');
+// const handleSearch = event => {
+//     const searchTerm = event.target.value.toLowerCase();
+// }
+
+// caption.forEach(caption => {
+//     const text = caption.textContent.toLowerCase();
+//     const box = caption.parentElement;
+
+//     if( text.indexOf(searchTerm) > -1) {
+//         box.style.display = "block";
+//     } else {
+//         box.style.display = "none";
+//     }
+// });
+
+// search.addEventListener('keyup', handleSearch);
