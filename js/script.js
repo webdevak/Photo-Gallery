4d79842a99560b9ec5446064e7c1c4f46f74ff48
Hakanni <66316345+webdev32@users.noreply.github.com>
2020-09-09 12:03:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-const search = document.querySelector('#search');
-const caption = document.querySelectorAll('.gallery a');
+const search = document.querySelector("#search");
+const caption = document.querySelectorAll(".gallery a");
 
-const handleSearch = event => {
+const handleSearch = (event) => {
+  const searchTerm = event.target.value.toLowerCase();
 
-    const searchTerm = event.target.value.toLowerCase();
-
-
-caption.forEach(caption => {
+  caption.forEach((caption) => {
     const text = caption.textContent.toLowerCase();
-    const box = caption.parentElement;
+    const box = caption.firstElementChild;
 
-    if( text.indexOf(searchTerm) > -1) {
-        box.style.display = "data-caption";
+    if (text.indexOf(searchTerm) > -1) {
+      box.style.display = "block";
     } else {
-        box.style.display = "none";
+      box.style.display = "none";
     }
-});
-
+  });
 };
 
-search.addEventListener('keyup', 'handleSearch');
+// search.addEventListener('keyup', 'handleSearch');
 
+const search = new Filter("search", "data-caption");
