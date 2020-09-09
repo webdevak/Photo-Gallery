5dfcf78870fed1d647efb6f9763a4ccd0705bb33
Hakanni <66316345+webdev32@users.noreply.github.com>
2020-09-09 12:03:43 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
-baguetteBox.run('.gallery');
-
-const search = new Filter('search', 'data-caption');
+baguetteBox.run(".gallery");
 
 
 
+const searchBar = document.querySelector("#search");
+
+
+searchBar.addEventListener("keyup", function (e) {
+  const term = e.target.value.toLowerCase();
+  const caption = document.querySelectorAll(".gallery a");
+
+  for (i = 0; i < caption.length; i++) {
+    const text = document.getElementsByTagName('a')[i].getAttribute('data-caption').toLowerCase();
+    const imageSearch = text.indexOf(term)
+    if (imageSearch > -1) {
+      (document.getElementsByTagName('a')[i]).style.display = 'block';
+    } else {
+        (document.getElementsByTagName('a')[i]).style.display = 'none';
+    }
+  };
+});
