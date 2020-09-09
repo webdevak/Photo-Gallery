6a9237256fa7484d9c1818abecd9a4311bdb1a2f
Hakanni <66316345+webdev32@users.noreply.github.com>
2020-09-09 16:02:27 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,14 +8,15 @@ const searchBar = document.querySelector("#search");
 searchBar.addEventListener("keyup", function (e) {
   const term = e.target.value.toLowerCase();
   const caption = document.querySelectorAll(".gallery a");
+  const imgText = document.getElementsByTagName('a');
 
   for (i = 0; i < caption.length; i++) {
-    const text = document.getElementsByTagName('a')[i].getAttribute('data-caption').toLowerCase();
+    const text = imgText[i].getAttribute('data-caption').toLowerCase();
     const imageSearch = text.indexOf(term)
     if (imageSearch > -1) {
-      (document.getElementsByTagName('a')[i]).style.display = 'block';
+      imgText[i].style.display = 'block';
     } else {
-        (document.getElementsByTagName('a')[i]).style.display = 'none';
+        imgText[i].style.display = 'none';
     }
-  };
+  }
 });
